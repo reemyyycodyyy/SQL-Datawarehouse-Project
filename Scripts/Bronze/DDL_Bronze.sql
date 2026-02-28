ddf7e61d1aa34e078d063e5e6278a5b4a2e374ff
reemyyycodyyy <tetobil22@gmail.com>
2026-02-28 16:28:00 +0200

    Update DDL_Bronze.sql

--- Scripts/Bronze/DDL_Bronze.sql
+++ Scripts/Bronze/DDL_Bronze.sql
@@ -1,1 +1,86 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Layer Tables
+===============================================================================
+Script Purpose:
+    This script creates tables inside the 'Bronze' schema.
+    The script first checks if tables already exist. If they do, they are
+    dropped and recreated to maintain schema consistency.
 
+Execution Instructions:
+    - Run this script to (re)define the Bronze layer table structures.
+    - Ensure you are connected to the correct database before execution.
+===============================================================================
+*/
+
+if object_id('Bronze.crm_cust_inf', 'u') is not null 
+	drop table Bronze.crm_cust_inf ; 
+
+create table Bronze.crm_cust_inf(
+	cst_id int,
+	cst_key nvarchar(50),
+	cst_fname nvarchar(50),
+	cst_lname nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date date 
+) ; 
+
+
+if object_id('Bronze.crm_prd_info', 'u') is not null 
+	drop table Bronze.crm_prd_info ; 
+
+create table Bronze.crm_prd_info(
+prd_id int,
+prd_key nvarchar(50),
+prd_nm nvarchar(50),
+prd_cost float,
+prd_line nvarchar(50),
+prd_start_dt date,
+prd_end_dt date
+);
+
+
+if object_id('Bronze.crm_sales_details', 'u') is not null 
+	drop table Bronze.crm_sales_details ; 
+
+create table Bronze.crm_sales_details(
+sls_ord_num nvarchar(50),
+sls_prd_key nvarchar(50),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int
+);
+
+
+if object_id('Bronze.erp_cust_az12', 'u') is not null 
+	drop table Bronze.erp_cust_az12 ; 
+
+create table Bronze.erp_cust_az12(
+cid nvarchar(50),
+bdate date,
+gen nvarchar(50)
+);
+
+
+if object_id('Bronze.erp_loc_a101', 'u') is not null 
+	drop table Bronze.erp_loc_a101 ; 
+
+create table Bronze.erp_loc_a101(
+cid nvarchar(50),
+cntry nvarchar(50)
+);
+
+
+if object_id('Bronze.erp_px_cat_g1v2', 'u') is not null 
+	drop table Bronze.erp_px_cat_g1v2 ; 
+
+create table Bronze.erp_px_cat_g1v2(
+id nvarchar(50),
+cat nvarchar(50),
+subcat nvarchar(50),
+maintenance nvarchar(50)
+);
